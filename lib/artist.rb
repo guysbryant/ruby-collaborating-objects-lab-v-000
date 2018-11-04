2f3184519403d462532ceb570819b7cb86d50d75
Guy Bryant <guy.s.bryant@gmail.com>
2018-11-04 21:36:11 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,9 @@
 class Artist
+<<<<<<< HEAD
   attr_accessor :name, :songs
+=======
+  attr_accessor :all, :name
+>>>>>>> c1f6594a98ac249665c9422d8134b83193c6818d
   @@all = []
   
   def initialize(name)
@@ -7,6 +11,7 @@ class Artist
     @songs = []
   end
   
+<<<<<<< HEAD
   def self.find_or_create_by_name(name)
     @@all.each {|artist| return artist if artist.name == name}
     name = Artist.new(name)
@@ -20,14 +25,26 @@ class Artist
   
   def self.all
     @@all
+=======
+  def find_or_create_by_name(name)
+    @@all.each {|artist| return artist if artist.name == name}
+    name = Artist.new(name)
+  end
+  
+  def add_song(song)
+    @songs << song
+>>>>>>> c1f6594a98ac249665c9422d8134b83193c6818d
   end
   
   def save
     @@all << self
   end
   
+<<<<<<< HEAD
   def print_songs
     @songs.each{|song| puts song.name}
   end
   
+=======
+>>>>>>> c1f6594a98ac249665c9422d8134b83193c6818d
 end
