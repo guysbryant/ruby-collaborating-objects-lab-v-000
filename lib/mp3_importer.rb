0959b5453042ca1121dba4ef222eb730b0542517
Guy Bryant <guy.s.bryant@gmail.com>
2018-11-04 21:40:47 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,5 +1,4 @@
 class MP3Importer
-<<<<<<< HEAD
   attr_accessor :path
   
   def initialize(path)
@@ -12,19 +11,5 @@ class MP3Importer
   
   def import
     files.select{|file| file[-4] == '.'}.each{|file| Song.new_by_filename(file)}
-=======
-  attr_accessor :path, :files_in_folder
-  
-  def initialize(path)
-    import(files(path))
-  end
-  
-  def files(path)
-    @files_in_folder = Dir.entires(path).select{|file| file[-4] == '.'}
-  end
-  
-  def import(files)
-    @files_in_folder.each{|file| Song.new(file)}
->>>>>>> c1f6594a98ac249665c9422d8134b83193c6818d
   end
 end
